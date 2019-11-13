16dd92aa0637e2b240514b4efe31dc60c1c10265
AnaMijailovic <anaam1997@gmail.com>
2019-11-13 17:58:15 +0100

    Spring project

---
+++ scientific_papers/scientific_papers/src/main/java/com/ftn/scientific_papers/ScientificPapersApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.scientific_papers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificPapersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScientificPapersApplication.class, args);
+	}
+
+}
---
+++ scientific_papers/scientific_papers/src/test/java/com/ftn/scientific_papers/ScientificPapersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.scientific_papers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificPapersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
