e509ba58348db22f24ac0184d840a1caabfa5203
Dusan Stevic <stevicdule@gmail.com>
2019-12-22 11:40:55 +0100

    storing cover-letter data in XML database

---
+++ scientific_papers/scientific_papers/src/main/java/com/ftn/scientific_papers/service/CoverLetterService.java
@@ -0,0 +1,36 @@
+package com.ftn.scientific_papers.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.xmldb.api.modules.XMLResource;
+
+import com.ftn.scientific_papers.dom.DOMParser;
+import com.ftn.scientific_papers.repository.CoverLetterRepository;
+import com.ftn.scientific_papers.repository.ScientificPaperRepository;
+
+@Service
+public class CoverLetterService {
+	
+	static String spSchemaPath = "src/main/resources/xsd/cover_letter.xsd";
+	
+	@Autowired
+	private CoverLetterRepository spRepository;
+
+	public XMLResource findOne(String id) throws Exception {
+		
+		return spRepository.findOne(id);
+	}
+
+	public void save(String scientificPaperXml) throws Exception {
+		
+	   // SAXParseExcetion is thrown when xml is not valid
+       Document document =  DOMParser.buildDocument(scientificPaperXml, spSchemaPath);
+   
+       	// TODO Generate ids for chapters, paragraphs etc. 
+        // TODO Check chapter levels (max is 5)
+       
+	   spRepository.save(scientificPaperXml);
+
+	}
+}
