8b3520eeb4cd8e5d46f773bd5c442e86a64994f8
AnaMijailovic <anaam1997@gmail.com>
2020-06-14 14:48:39 +0200

    withdraw paper

---
+++ scientific_papers/scientific_papers/src/main/java/com/ftn/scientific_papers/controller/PublishingProcessController.java
@@ -0,0 +1,29 @@
+package com.ftn.scientific_papers.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ftn.scientific_papers.service.PublishingProcessService;
+
+@RestController
+@RequestMapping(value = "/api/publishingProcess")
+@CrossOrigin()
+public class PublishingProcessController {
+
+	@Autowired
+	private PublishingProcessService publishingProcessService;
+
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	private ResponseEntity<String> findOne(@RequestParam(("paperId")) String paperId) throws Exception {
+		String resource = publishingProcessService.findOneByPaperId(paperId);
+		return new ResponseEntity<>(resource, HttpStatus.OK);
+	}
+
+}
